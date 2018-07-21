0a268f730534a6f98ad49edf2d4908e0cac8c7e7
Mariyan Iliev <mapo441@gmail.com>
2018-07-21 22:33:08 +0300

    Create a api request, add styles to the name of the album and the name of the artist , add artist img, and add a album img, create the view scrollable

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, } from 'react-native';
+import { ScrollView } from 'react-native';
 import AlbumDetail from './albumDetail';
 
 class AlbumList extends Component {
@@ -24,9 +24,9 @@ class AlbumList extends Component {
   render() {
     console.log(this.state);
     return (
-      <View>
+      <ScrollView  >
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
